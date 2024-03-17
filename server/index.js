8a43a0c43b2b8a78f8b3c9254b08b0de682359a2
Ugochukwu Benjamin <benstar531@gmail.com>
2024-03-17 18:43:55 +0100

    configure websocket connection

--- server/index.js
+++ server/index.js
@@ -3,17 +3,27 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const { Server } = require('socket.io');
 
 const app = express();
 
 // Connect Database
 connectDB();
 
+// Enable Cross-Origin Resource Sharing
 app.use(cors());
 
+// parse incoming request bodies in JSON format
 app.use(express.json({ extended: false }));
 
 const server = http.createServer(app);
+
+const io = new Server(server, {
+	cors: {
+		origin: '*',
+		methods: ['GET', 'POST'],
+	},
+});
 
 // home route
 app.get('/', (req, res) => {
@@ -23,5 +33,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-	console.log(`Server is running at http://localhost:${PORT}`);
+	console.log(`Hurray! Our Server is running at port:${PORT}`);
 });
